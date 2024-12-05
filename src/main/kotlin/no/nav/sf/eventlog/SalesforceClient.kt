248c44bfa997e129b9e15ef4d35df548903a6be5
Björn Hägglund <bjorn.hagglund@nav.no>
2024-12-05 16:04:21 +0100

    Example secure log

--- src/main/kotlin/no/nav/sf/eventlog/SalesforceClient.kt
+++ src/main/kotlin/no/nav/sf/eventlog/SalesforceClient.kt
@@ -3,6 +3,7 @@ package no.nav.sf.eventlog
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.sf.eventlog.token.AccessTokenHandler
 import no.nav.sf.eventlog.token.DefaultAccessTokenHandler
 import org.apache.commons.csv.CSVFormat
@@ -10,28 +11,52 @@ import org.apache.commons.csv.CSVParser
 import org.http4k.client.ApacheClient
 import org.http4k.core.Method
 import org.http4k.core.Request
-import org.slf4j.Marker
-import org.slf4j.MarkerFactory
 import java.io.File
 import java.io.StringReader
 
 class SalesforceClient(private val accessTokenHandler: AccessTokenHandler = DefaultAccessTokenHandler()) {
     private val log = KotlinLogging.logger { }
 
-    private val SECURE: Marker = MarkerFactory.getMarker("SECURE_LOG")
-
     private val apiVersion = env(config_SALESFORCE_API_VERSION)
 
     private val client = ApacheClient()
 
     fun fetchEventLog(eventType: EventType) {
+        val logFiles = fetchLogFiles(eventType)
+
+        val first = logFiles.first()
+        val capturedEvents = fetchLogFileContentAsJson(first)
+        log.info { "Will log ${capturedEvents.size} events to secure logs" }
+
+        capturedEvents.forEach {
+            withLoggingContext(
+                mapOf(
+                    "EVENT_TYPE" to it["EVENT_TYPE"].asString,
+                    "TIMESTAMP" to it["TIMESTAMP"].asString,
+                    "TIMESTAMP" to it["TIMESTAMP_DERIVED"].asString,
+                    "REQUEST_ID" to it["REQUEST_ID"].asString,
+                    "ORGANIZATION_ID" to it["ORGANIZATION_ID"].asString,
+                    "USER_ID" to it["USER_ID"].asString,
+                    "EXCEPTION_TYPE" to it["EXCEPTION_TYPE"].asString,
+                    "STACK_TRACE" to it["STACK_TRACE"].asString,
+                    "EXCEPTION_CATEGORY" to it["EXCEPTION_CATEGORY"].asString,
+                    "USER_ID_DERIVED" to it["USER_ID_DERIVED"].asString,
+                )
+            ) {
+                // log.info { it["EXCEPTION_MESSAGE"].asString }
+                log.error(SECURE, it["EXCEPTION_MESSAGE"].asString)
+            }
+        }
+    }
+
+    private fun fetchLogFiles(eventType: EventType): List<String> {
         val soqlQuery = "SELECT Id, EventType, LogFile, LogDate FROM EventLogFile WHERE EventType='${eventType.name}'"
         val encodedQuery = soqlQuery.replace(" ", "+") // URL-encode the query
 
         var done = false
         var nextRecordsUrl = "/services/data/$apiVersion/query?q=$encodedQuery"
 
-        val logFiles: MutableList<String> = mutableListOf()
+        val result: MutableList<String> = mutableListOf()
 
         while (!done) {
             val request = Request(Method.GET, accessTokenHandler.instanceUrl + nextRecordsUrl)
@@ -41,9 +66,9 @@ class SalesforceClient(private val accessTokenHandler: AccessTokenHandler = Defa
             if (response.status.successful) {
                 val obj = JsonParser.parseString(response.bodyString()).asJsonObject
                 val recordEntries = obj["records"].asJsonArray
-                logFiles.addAll(recordEntries.map { it.asJsonObject["LogFile"].asString })
+                result.addAll(recordEntries.map { it.asJsonObject["LogFile"].asString })
                 val totalSize = obj["totalSize"].asInt
-                log.info { "Fetched ${logFiles.count()} of $totalSize log files for ${eventType.name}" }
+                log.info { "Fetched ${result.count()} of $totalSize log files for ${eventType.name}" }
                 done = obj["done"].asBoolean
                 if (!done) nextRecordsUrl = obj["nextRecordsUrl"].asString
             } else {
@@ -51,18 +76,8 @@ class SalesforceClient(private val accessTokenHandler: AccessTokenHandler = Defa
                 done = true
             }
         }
-        val first = logFiles.first()
-        val capturedEvents = fetchLogFileContentAsJson(first)
-        log.info { "Will log ${capturedEvents.size} events to secure logs" }
-        capturedEvents.forEach {
-            log.error(
-                SECURE,
-                "Example forwarded message from first file: " +
-                    it["EXCEPTION_TYPE"].asString +
-                    ":" +
-                    it["EXCEPTION_MESSAGE"].asString
-            )
-        }
+
+        return result
     }
 
     private fun fetchLogFileContentAsJson(logFileUrl: String): List<JsonObject> {
--- src/main/kotlin/no/nav/sf/eventlog/utils.kt
+++ src/main/kotlin/no/nav/sf/eventlog/utils.kt
@@ -1,6 +1,10 @@
 package no.nav.sf.eventlog
 
+import org.slf4j.Marker
+import org.slf4j.MarkerFactory
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
+val SECURE: Marker = MarkerFactory.getMarker("SECURE_LOG")
 
 val currentDateTime: String get() = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
