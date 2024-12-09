b7889cf1f079a4ee067b3bb99d822f43b03ca8f6
Björn Hägglund <bjorn.hagglund@nav.no>
2024-12-09 14:14:16 +0100

    Try with other format on derived timestamp

--- src/main/kotlin/no/nav/sf/eventlog/CustomLogstashEncoder.kt
+++ src/main/kotlin/no/nav/sf/eventlog/CustomLogstashEncoder.kt
@@ -4,6 +4,9 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import com.google.gson.JsonParser
 import net.logstash.logback.encoder.LogstashEncoder
 import java.nio.charset.StandardCharsets
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class CustomLogstashEncoder : LogstashEncoder() {
 
@@ -18,11 +21,13 @@ class CustomLogstashEncoder : LogstashEncoder() {
         // Access TIMESTAMP_DERIVED from MDC and replace @timestamp
         val timestampDerived = event.mdcPropertyMap[FIELD_TO_REPLACE_TIMESTAMP]
         if (timestampDerived != null) {
+            val parsedTimestamp = ZonedDateTime.parse(timestampDerived)
+            val adjustedTimestamp = parsedTimestamp.withZoneSameInstant(ZoneOffset.ofHours(1)) // Adjust to +01:00
             jsonObject.addProperty(FIELD_TO_STORE_EVENT_LOG_TIMESTAMP_ON_REPLACEMENT, jsonObject["@timestamp"].asString)
-            jsonObject.addProperty("@timestamp", timestampDerived)
+            jsonObject.addProperty("@timestamp", adjustedTimestamp.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
         }
 
-        jsonObject.addProperty("message", "Mod.v01 " + jsonObject["message"].toString())
+        jsonObject.addProperty("message", "Mod.v02 " + jsonObject["message"].toString())
 
         // Return the modified JSON as a byte array
         return (jsonObject.toString() + "\n").toByteArray(StandardCharsets.UTF_8)
