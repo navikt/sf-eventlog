5f4f74fcc853b3e8780c59338299be9c033b0ecd
Björn Hägglund <bjorn.hagglund@nav.no>
2025-03-17 12:54:05 +0100

    Update AppRecordPartial model

--- src/main/kotlin/no/nav/sf/eventlog/salesforce/AppRecordPartial.kt
+++ src/main/kotlin/no/nav/sf/eventlog/salesforce/AppRecordPartial.kt
@@ -5,5 +5,6 @@ data class AppRecordPartial(
     val Log_Level__c: String,
     val Application_Domain__c: String,
     val Source_Class__c: String,
+    val Source_Function__c: String,
     val UUID__c: String
 )
